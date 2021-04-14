7225400d7b43f61354554f50d30a12876e112784
W0430313 <W0430313@nscc.ca>
2021-04-14 16:20:35 -0300

    Refactored the models to have all shared fields in Critter.cs

--- Critterpedia/Critterpedia.App/Models/SeaCreature.cs
+++ Critterpedia/Critterpedia.App/Models/SeaCreature.cs
@@ -9,26 +9,6 @@ namespace Critterpedia.App.Models
 {
     class SeaCreature : Critter
     {
-        [JsonProperty(PropertyName = "id")]
-        public int id { get; set; }
-
-        [JsonProperty(PropertyName = "file-name")]
-        public string name { get; set; }
-
-        [JsonProperty(PropertyName = "price")]
-        public int price { get; set; }
-
-        [JsonProperty(PropertyName = "catch-phrase")]
-        public string catchPhrase { get; set; }
-
-        [JsonProperty(PropertyName = "museum-phrase")]
-        public string museumPhrase { get; set; }
-
-        [JsonProperty(PropertyName = "image_uri")]
-        public string imageUri { get; set; }
-
-        [JsonProperty(PropertyName = "icon_uri")]
-        public string iconUri { get; set; }
 
         [JsonProperty(PropertyName = "shadow")]
         public string shadow { get; set; }
@@ -36,10 +16,5 @@ namespace Critterpedia.App.Models
         [JsonProperty(PropertyName = "speed")]
         public string speed { get; set; }
 
-        [JsonProperty(PropertyName = "availability")]
-        public Availability availability { get; set; }
-
-        [JsonProperty(PropertyName = "name")]
-        public Name names { get; set; }
     }
 }
