ca0d37bed1fc957d7bd4bc1e5eb9244e9195db4f
Kieran London <W0429281@campus.nscc.ca>
2021-04-14 14:18:25 -0300

    Critters implemeted but commented out to avoid issues

--- Critterpedia/Critterpedia.App/ViewModels/Interfaces/ICritterViewModel.cs
+++ Critterpedia/Critterpedia.App/ViewModels/Interfaces/ICritterViewModel.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Critterpedia.App.Models;
-using Critterpedia.App.Commands;
 using Critterpedia.App.Repository;
-using Windows.Storage;
-using System.Diagnostics;
 
 namespace Critterpedia.App.ViewModels
 {
